15ac04ac21e7cfe1aeb6735bcb62a8c336a1f3d4
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2023-01-26 14:13:04 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function togglenav(){
+    sidenav = document.getElementById('sidenav').style; 
+    if (sidenav.display == "none"){
+    sidenav.display = 'block'
+    } 
+    else{
+        sidenav.display = 'none'
+    }
+}
